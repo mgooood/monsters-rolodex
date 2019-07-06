2408a76d51b23e94aac3207f9ace7a56a293fe89
Mark Good <mark.gooood@gmail.com>
2019-07-06 13:06:22 -0400

    Updates

---
+++ precache-manifest.ba8ecdeb788aa9dc2da18d22cc3ca70d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0fef0ebeadf94aa5aadfbb621b6b51",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2f6ddfadff71e0cdb1a5",
+    "url": "/monsters-rolodex/static/css/main.fe2ed859.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2f6ddfadff71e0cdb1a5",
+    "url": "/monsters-rolodex/static/js/main.4c83e3c2.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
